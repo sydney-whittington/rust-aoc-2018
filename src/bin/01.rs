42e0c2b4adc4f1157c843eb59e323111df282ac0
Sydney Whittington <sydney.whittington@mooreintegrity.com>
2024-08-15 15:30:51 -0500

    day 1 part 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,10 +1,19 @@
 advent_of_code::solution!(1);
 
-pub fn part_one(input: &str) -> Option<u32> {
-    None
+use nom::{
+    character::complete::{i32, newline}, multi::separated_list0, IResult
+};
+
+fn parser(i: &str) -> IResult<&str, Vec<i32>> {
+    separated_list0(newline, i32)(i)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+pub fn part_one(input: &str) -> Option<i32> {
+    let (_, nums) = parser(input).unwrap();
+    Some(nums.iter().sum())
+}
+
+pub fn part_two(input: &str) -> Option<i32> {
     None
 }
 
@@ -15,7 +24,11 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(3));
+        let result = part_one(&advent_of_code::template::read_file_part("examples", DAY, 1));
+        assert_eq!(result, Some(0));
+        let result = part_one(&advent_of_code::template::read_file_part("examples", DAY, 2));
+        assert_eq!(result, Some(-6));
     }
 
     #[test]
