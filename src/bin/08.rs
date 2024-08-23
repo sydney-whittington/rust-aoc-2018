8ea2bea98f8b998b31bfd98a9ade747dce4ab5e9
Sydney Whittington <sydney.whittington@mooreintegrity.com>
2024-08-23 14:46:37 -0500

    day 8 part 2

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,6 +1,6 @@
 advent_of_code::solution!(8);
 
-use advent_of_code::{number, number_usize};
+use advent_of_code::number_usize;
 use nom::{
     bytes::complete::tag,
     character::complete::multispace1,
@@ -12,16 +12,31 @@ use nom::{
 #[derive(Debug)]
 struct Node {
     children: Vec<Node>,
-    metadata: Vec<u32>,
+    metadata: Vec<usize>,
 }
 
 impl Node {
-    fn sum_metadata(&self) -> u32 {
-        self.metadata.iter().sum::<u32>()
+    fn sum_metadata(&self) -> usize {
+        self.metadata.iter().sum::<usize>()
             + self
                 .children
                 .iter()
                 .fold(0, |acc, c| c.sum_metadata() + acc)
+    }
+
+    fn sum_values(&self) -> usize {
+        if self.children.len() > 0 {
+            let mut value = 0;
+            for pointer in self.metadata.iter() {
+                // subtract 1 for 1-indexed data
+                if let Some(n) = self.children.get(*pointer-1) {
+                    value += n.sum_values();
+                }
+            }
+            value
+        } else {
+            self.sum_metadata()
+        }
     }
 }
 
@@ -32,18 +47,19 @@ fn node(i: &str) -> IResult<&str, Node> {
     ))(i)?;
     let (i, children) = count(node, child_count)(i)?;
     // last entry is a newline so look for space or newline
-    let (i, metadata) = count(terminated(number, multispace1), metadata_count)(i)?;
+    let (i, metadata) = count(terminated(number_usize, multispace1), metadata_count)(i)?;
 
     Ok((i, Node { children, metadata }))
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
+pub fn part_one(input: &str) -> Option<usize> {
     let (_, node) = node(input).unwrap();
     Some(node.sum_metadata())
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<usize> {
+    let (_, node) = node(input).unwrap();
+    Some(node.sum_values())
 }
 
 #[cfg(test)]
@@ -59,6 +75,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(66));
     }
 }
