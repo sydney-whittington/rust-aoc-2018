66fd6c7b1a443f8c8ee370bf42d3f677ccb27bf8
Sydney Whittington <sydney.whittington@mooreintegrity.com>
2024-08-30 13:09:46 -0500

    day 14 part 2

--- src/bin/13.rs
+++ src/bin/13.rs
@@ -1,4 +1,8 @@
-use std::{collections::{HashMap, HashSet}, error::Error, fmt};
+use std::{
+    collections::{HashMap, HashSet},
+    error::Error,
+    fmt,
+};
 
 use advent_of_code::{Coordinate, Output};
 use itertools::Itertools;
@@ -250,7 +254,9 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file_part("examples", DAY, 2));
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
         assert_eq!(result, Some(Output(6, 4)));
     }
 }
--- src/bin/14.rs
+++ src/bin/14.rs
@@ -8,7 +8,7 @@ struct Recipes {
     elf_1_idx: usize,
     elf_2_idx: usize,
     pub board: Vec<usize>,
-    cached: VecDeque<usize>
+    cached: VecDeque<usize>,
 }
 
 impl Iterator for Recipes {
@@ -16,17 +16,21 @@ impl Iterator for Recipes {
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.cached.len() > 0 {
-            return self.cached.pop_front()
+            return self.cached.pop_front();
         }
 
         let scores = self.board[self.elf_1_idx] + self.board[self.elf_2_idx];
         // https://stackoverflow.com/questions/41536479/how-do-i-split-an-integer-into-individual-digits
-        let digits: Vec<usize> = scores.to_string().chars().map(|d| d.to_digit(10).unwrap().try_into().unwrap()).collect();   
+        let digits: Vec<usize> = scores
+            .to_string()
+            .chars()
+            .map(|d| d.to_digit(10).unwrap().try_into().unwrap())
+            .collect();
 
         self.board.extend(digits.iter());
         self.elf_1_idx = (1 + self.board[self.elf_1_idx] + self.elf_1_idx) % self.board.len();
         self.elf_2_idx = (1 + self.board[self.elf_2_idx] + self.elf_2_idx) % self.board.len();
-        
+
         self.cached.extend(digits);
         Some(self.cached.pop_front().unwrap())
     }
@@ -37,11 +41,9 @@ impl fmt::Display for Recipes {
         for (i, val) in enumerate(self.board.iter()) {
             if i == self.elf_1_idx {
                 write!(f, "({}) ", val).unwrap();
-            }
-            else if i == self.elf_2_idx {
+            } else if i == self.elf_2_idx {
                 write!(f, "[{}] ", val).unwrap();
-            }
-            else {
+            } else {
                 write!(f, " {}  ", val).unwrap();
             }
         }
@@ -50,18 +52,53 @@ impl fmt::Display for Recipes {
 }
 
 fn new_recipes() -> Recipes {
-    Recipes { elf_1_idx: 0, elf_2_idx: 1, board: vec![3, 7, 1, 0], cached: vec![3, 7, 1, 0].into()}
+    Recipes {
+        elf_1_idx: 0,
+        elf_2_idx: 1,
+        board: vec![3, 7, 1, 0],
+        cached: vec![3, 7, 1, 0].into(),
+    }
 }
 
-pub fn part_one(input: &str) -> Option<String> {    
+pub fn part_one(input: &str) -> Option<String> {
     let num_recipes = input.trim().parse::<usize>().unwrap();
     let recipes = new_recipes();
 
     Some(recipes.skip(num_recipes).take(10).join(""))
 }
 
-pub fn part_two(_input: &str) -> Option<u64> {
-    None
+pub fn part_two_test(input: &str) -> Option<usize> {
+    let num_recipes: (usize, usize, usize, usize, usize) = input
+        .trim()
+        .chars()
+        .map(|i| i.to_digit(10).unwrap().try_into().unwrap())
+        .collect_tuple()
+        .unwrap();
+    let recipes = new_recipes();
+
+    let (i, _) = recipes
+        .tuple_windows()
+        .find_position(|w: &(usize, usize, usize, usize, usize)| *w == num_recipes)
+        .unwrap();
+    Some(i)
+}
+
+// the test inputs were 5 digits, but the actual is 6
+// not sure if there's a way to parametrically set the tuple size for the substring, oh well
+pub fn part_two(input: &str) -> Option<usize> {
+    let num_recipes: (usize, usize, usize, usize, usize, usize) = input
+        .trim()
+        .chars()
+        .map(|i| i.to_digit(10).unwrap().try_into().unwrap())
+        .collect_tuple()
+        .unwrap();
+    let recipes = new_recipes();
+
+    let (i, _) = recipes
+        .tuple_windows()
+        .find_position(|w: &(usize, usize, usize, usize, usize, usize)| *w == num_recipes)
+        .unwrap();
+    Some(i)
 }
 
 #[cfg(test)]
@@ -94,7 +131,25 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let result = part_two_test("51589");
+        assert_eq!(result, Some(9));
+    }
+
+    #[test]
+    fn test_part_two_a() {
+        let result = part_two_test("01245");
+        assert_eq!(result, Some(5));
+    }
+
+    #[test]
+    fn test_part_two_b() {
+        let result = part_two_test("92510");
+        assert_eq!(result, Some(18));
+    }
+
+    #[test]
+    fn test_part_two_c() {
+        let result = part_two_test("59414");
+        assert_eq!(result, Some(2018));
     }
 }
